5916cc7946118724fdefbda7825399bee8cd331d
Alexander-its <GO_PB333666@mail.ru>
2022-11-14 15:52:56 +0300

    !

--- individ-8.py
+++ individ-8.py
@@ -4,14 +4,18 @@
 #«Площадь круга равна = <число>». В строке выведите числовое значение с точностью до
 #сотых. Примените декоратор к функции и вызовите декорированную функцию.
 
-def print_info(func):
-    def f(*args, **kwargs):
-        result = func(*args, **kwargs)
-        print(f'Площадь круга равна = {result:1.2f}')
-    return f
-@print_info
-def area(x):
-    return 3.1415926 * x * x
- 
-r = float(input('Введите радиус'))
-area(r)
+#!/usr/bin/env python3
+# -*- coding: utf -8 -*-
+
+if __name__ == '__main__':
+    def print_info(func):
+        def f(*args, **kwargs):
+            result = func(*args, **kwargs)
+            print(f'Площадь круга равна = {result:1.2f}')
+        return f
+    @print_info
+    def area(x):
+        return 3.1415926 * x * x
+
+    r = float(input('Введите радиус'))
+    area(r)
